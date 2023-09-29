9b7fcb58fb36896a99835a284d0e9df2ad15d52f
DeytonGarcia <136906803+DeytonGarcia@users.noreply.github.com>
2023-09-29 13:44:39 -0500

    Add files via upload

---
+++ 10_DeytonGarcia_POO/src/pkg10_deytongarcia_poo/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg10_deytongarcia_poo;
+
+/**
+ *
+ * @author deyto
+ */
+public class Calculadora {
+    
+   //Atributos 
+     int a =12;
+     int b= 14;
+     int mul;
+     
+   //metodo
+     public void multiplicar(){
+         mul= a*b ;
+         System.out.print(b);
+     }
+}
